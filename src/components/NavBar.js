ee96a9e551188bbb3e8b8917ba99749fe457c787
osbrown1 <socknot@live.com>
2023-04-29 15:32:10 -0700

    Used svg's to import linkedin and github icons from the Icon component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,6 +2,10 @@ import Link from 'next/link'
 import { useRouter } from 'next/router'
 import React from 'react'
 import Logo from './Logo'
+import { GithubIcon, LinkedInIcon } from './Icons'
+
+
+
 
 
 const CustomLink = ({href, title, className=""}) => {
@@ -19,32 +23,26 @@ const CustomLink = ({href, title, className=""}) => {
 }
 
 
-const NavBar = () => {
-  return (
-    <header className='w-full px-32 py-8 font-medium flex items-center justify-between'>
-      <nav>
-        <CustomLink href="/" title="Home" className='mr-4'/>
-        <CustomLink href="/about" title="About" className='mx-4'/>
-        <CustomLink href="/projects" title="Projects" className='mx-4'/> 
-        <CustomLink href="/articles" title="Articles" className='ml-4'/> 
-      </nav>
-      
-      <nav>
-      <Link href="/" target={"_blank"}>T</Link>
-      <Link href="/" target={"_blank"}>T</Link>
-      <Link href="/" target={"_blank"}>T</Link>
-      <Link href="/" target={"_blank"}>T</Link>
-      <Link href="/" target={"_blank"}>T</Link>
-      <Link href="/" target={"_blank"}>T</Link>
-      </nav>
+const NavBar = () => (
+  <header className='w-full px-32 py-8 font-medium flex items-center justify-between'>
+    <nav>
+      <CustomLink href="/" title="Home" className='mr-4' />
+      <CustomLink href="/about" title="About" className='mx-4' />
+      <CustomLink href="/projects" title="Projects" className='mx-4' />
+      <CustomLink href="/articles" title="Articles" className='ml-4' />
+    </nav>
 
-       <div className='absolute left-[50%] top-2 translate-x-[-50%]'>
-       <Logo />
-      </div> 
+    <nav>
+      <Link href="/" target={"_blank"}><GithubIcon /></Link>
+      <Link href="/" target={"_blank"}><LinkedInIcon /></Link>
+    </nav>
 
-    </header>
-  );
-};
+    <div className='absolute left-[50%] top-2 translate-x-[-50%]'>
+      <Logo />
+    </div>
+
+  </header>
+);
 
 export default NavBar;
 
