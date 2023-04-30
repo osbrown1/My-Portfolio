5f18314c6f0fcc26e53ba4498fb095e2697028e6
osbrown1 <socknot@live.com>
2023-04-29 17:23:10 -0700

    debugging uploading image causing webpage load fails

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,6 +3,7 @@ import { useRouter } from 'next/router'
 import React from 'react'
 import Logo from './Logo'
 import { GithubIcon, LinkedInIcon } from './Icons'
+import {motion} from "framer-motion"
 
 
 
@@ -32,9 +33,9 @@ const NavBar = () => (
       <CustomLink href="/articles" title="Articles" className='ml-4' />
     </nav>
 
-    <nav>
-      <Link href="/" target={"_blank"}><GithubIcon /></Link>
-      <Link href="/" target={"_blank"}><LinkedInIcon /></Link>
+    <nav className='flex items-center justify-center flex-wrap'>
+      <motion.a href="https://github.com/osbrown1" target={"_blank"}whileHover={{y:-2}} whileTap={{scale:0.9}} className="w-6 mx-3"><GithubIcon /></motion.a>
+      <motion.a href="https://www.linkedin.com/in/obrwn/" target={"_blank"}whileHover={{y:-2}} whileTap={{scale:0.9}} className="w-6 mx-3"><LinkedInIcon /></motion.a>
     </nav>
 
     <div className='absolute left-[50%] top-2 translate-x-[-50%]'>
