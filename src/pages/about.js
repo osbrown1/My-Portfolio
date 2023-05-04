7e836a534aa88534ec06aab77d16114576926680
osbrown1 <socknot@live.com>
2023-05-04 11:06:13 -0700

    include animated statistics on my about page using motion values

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,37 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import Head from 'next/head'
 import AnimatedText from '@/components/AnimatedText'
 import Layout from '@/components/Layout'
+import profilePic from "../../public/images/profile/Profilepic.jpg"
+import Image from 'next/image'
+import { useInView, useMotionValue, useSpring } from 'framer-motion'
+
+
+const AnimatedNumbers = ({value}) => {
+const ref = useRef(null)
+
+const motionValue = useMotionValue(0);
+const springValue = useSpring(motionValue, { duration: 3000})
+const isInView = useInView(ref);
+
+useEffect(() => {
+  if(isInView){
+    motionValue.set(value);
+  }
+}, [isInView, value, motionValue])
+
+useEffect(() => {
+  springValue.on("change", (latest) => {
+    if(ref.current && latest.toFixed(0) <= value ){
+      ref.current.textContent = latest.toFixed(0);
+    }
+  })
+
+}, [springValue, value])
+
+  return <span ref={ref}></span>
+}
+
 
 const about = () => {
   return (
@@ -12,7 +42,7 @@ const about = () => {
     </Head>
     <main className='flex w-full flex-col items-center justify-center'>
       <Layout className='pt-16'>
-      <AnimatedText text="Passion Fuels Purpose!" />
+      <AnimatedText text="Passion Fuels Purpose!" className='mb-16'/>
       <div className='grid w-full grid-cols-8 gap-16'>
         <div className='col-span-3 flex flex-col items-start justify-start'>
           <h2 className='mb-4 text-lg font-bold uppercase text-dark/75'>About Me</h2>
@@ -33,7 +63,38 @@ every project I work on. I look forward to the opportunity to bring my skills an
         </p>
         </div>
 
+      <div className='col-span-3 relative h-max rounded-2x1 border-2 border-solid border-dark bg-light p-8'>
+        <div className='absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark' />
+        <Image src={profilePic} alt="OwenBrown" className="w-full h-auto rounded-2x1" />
       </div>
+
+      <div className='col-span-2 flex flex-col items-end justify-between'>
+      
+      <div className='flex flex-col items-end justify-center'>
+        <span className='inline-block text-7xl font-bold'>
+          <AnimatedNumbers value={10} />+
+        </span>
+        <h2 className='text-xl font-medium capitalize text-dark/75'>satisfied clients</h2>
+      </div>
+
+      <div className='flex flex-col items-end justify-center'>
+        <span className='inline-block text-7xl font-bold'>
+        <AnimatedNumbers value={50} />+
+        </span >
+        <h2 className='text-xl font-medium capitalize text-dark/75'>projects completed</h2>
+      </div>
+
+      <div className='flex flex-col items-end justify-center'>
+        <span className='inline-block text-7xl font-bold'>
+        <AnimatedNumbers value={2} />+
+        </span>
+        <h2 className='text-xl font-medium capitalize text-dark/75'>years of experience</h2>
+      </div>
+
+
+      </div> 
+
+     </div>
       </Layout>
     </main>
     </>
