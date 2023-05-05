57c599819642dbfd56d8c7bef35c2853ac4e2944
osbrown1 <socknot@live.com>
2023-05-05 10:37:47 -0700

    added animated line next to details

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import {motion, useScroll} from "framer-motion"
+import LiIcon from './LiIcon';
 
 const Details = ({ position, company, companyLink, time, address, work }) => {
   return (
     <li className="my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-between">
+
+
+      <LiIcon />
       <div>
         <h3 className="capitalize font-bold text-2xl">
           {position}&nbsp;
@@ -24,14 +29,27 @@ const Details = ({ position, company, companyLink, time, address, work }) => {
 }
 
 const Experience = () => {
+  const ref = useRef(null);
+  const {scrollYProgress} = useScroll(
+    {
+      target: ref,
+      offset: ["start end", "center start"]
+    }
+  )
   return (
     <div className='my-64'>
       <h2 className='font-bold text-8xl mb-32 w-full text-center'>
       Experience
       </h2>
 
-      <div className='w-[75%] mx-auto relative'>
-        <ul>
+      <div ref={ref} className='w-[75%] mx-auto relative'>
+
+      <motion.div
+      style={{scaleY: scrollYProgress}}
+      
+      className='absolute left-8 top-0 w-[4px] h-full bg-dark origin-top'/>
+
+        <ul className='w-full flex flex-col items-start justify-between ml-4'>
         <Details
         position="Front Desk Agent" company="Strathcona Hotel" 
         companyLink="https://strathconahotel.com" 
---
+++ src/components/LiIcon.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const LiIcon = () => {
+  return (
+   <figure>
+    <svg width="75" heigh="75" viewBox='0 0 100 100'>
+      <circle cx cy />
+      <circle />
+      <circle />
+    </svg>
+   </figure>
+  )
+}
+
+export default LiIcon
