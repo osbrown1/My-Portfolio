e1edefc958ef3cbb23b0081464186bf409ab789d
osbrown1 <socknot@live.com>
2023-05-10 17:10:40 -0700

    wip

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,6 +8,7 @@ import { useInView, useMotionValue, useSpring } from 'framer-motion'
 import Skills from '@/components/Skills'
 import Experience from '@/components/Experience'
 import Education from '@/components/Education'
+import TransitionEffect from '@/components/TransitionEffect'
 
 
 const AnimatedNumbers = ({value}) => {
@@ -43,6 +44,7 @@ const about = () => {
       <title>Owen Brown | About Page</title>
       <meta name='description' content="any description" />
     </Head>
+    <TransitionEffect />
     <main className='flex w-full flex-col items-center justify-center dark:text-light'>
       <Layout className='pt-16'>
       <AnimatedText text="Passion Fuels Purpose!" className='mb-16'/>
